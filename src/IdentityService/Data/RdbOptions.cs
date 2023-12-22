ef27f5d7444bbc86861eb190d5959f71b6bee867
arcturus-alloush <ahmad.alloush@live.com>
2023-12-22 21:21:14 +0100

    refactor: project structure, inital commit for identity service.

---
+++ src/IdentityService/Data/RdbOptions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace IdentityService.Data
+{
+    public class RdbOptions
+    {
+        public string Host { get; set; } = string.Empty;
+        public string DbName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+
+        public string GetPostgresConnectionString()
+        {
+            var userIdPart = $"User ID={this.UserName};";
+            var passwordPart = $"Password={this.Password};";
+            var serverPart = $"Server={this.Host};";
+            var databasePart = $"Database={this.DbName};";
+            var poolingPart = "Pooling=true;";
+
+            return userIdPart + passwordPart + serverPart + databasePart + poolingPart;
+        }
+    }
+}
